d458d420c8d5251977d2760ed16a568986fe7f98
pokotyamu <pokotyamu@gmail.com>
2014-12-05 14:31:26 +0900

    Create markdown setting

--- init.el
+++ init.el
@@ -74,6 +74,10 @@
 ;;; 終了時にオートセーブファイルを消す
 (setq delete-auto-save-files t)
 
+;;markdown記法の設定
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 ;;ruby & railsの基本的な設定
 (autoload 'ruby-mode "ruby-mode"
   "Mode for editing ruby source files" t)
@@ -133,3 +137,15 @@
     '(("xdvi" . ".dvi")
           ("ghostview\\|gv" . ".ps")
               ("acroread\\|pdf\\|Preview\\|open" . ".pdf")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-function-name-face ((t (:foreground "color-33")))))
