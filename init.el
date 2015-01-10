8190156474807b8ae4fc12a125c70fea24b2962b
pokotyamu <pokotyamu@gmail.com>
2015-01-10 16:31:01 +0900

    add web-mode for .erb

--- init.el
+++ init.el
@@ -108,9 +108,51 @@
     ;;;   (define-key ruby-mode-map "|" 'ruby-electric-bar)
     )
 
-;rabbit-mode
+;;erb-webmode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-html-offset   4)
+  (setq web-mode-css-offset    4)
+  (setq web-mode-script-offset 4)
+  (setq web-mode-php-offset    4)
+  (setq web-mode-java-offset   4)
+  (setq web-mode-asp-offset    4)
+  (setq indent-tabs-mode t)
+  (setq tab-width 4))
+(add-hook 'web-mode-hook 'web-mode-hook)
+;; 色の設定
+(custom-set-faces
+ '(web-mode-doctype-face
+   ((t (:foreground "#82AE46"))))
+ '(web-mode-html-tag-face
+   ((t (:foreground "#E6B422" :weight bold))))
+ '(web-mode-html-attr-name-face
+   ((t (:foreground "#C97586"))))
+ '(web-mode-html-attr-value-face
+   ((t (:foreground "#82AE46"))))
+ '(web-mode-comment-face
+   ((t (:foreground "#D9333F"))))
+ '(web-mode-server-comment-face
+   ((t (:foreground "#D9333F"))))
+ '(web-mode-css-rule-face
+   ((t (:foreground "#A0D8EF"))))
+ '(web-mode-css-pseudo-class-face
+   ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-at-rule-face
+   ((t (:foreground "#FF7F00"))))
+ )
+
+                                        ;rabbit-mode
 (when (locate-library "rabbit-mode")
-(autoload 'rabbit-mode "rabbit-mode" "major mode for Rabbit" nil t)
+  (autoload 'rabbit-mode "rabbit-mode" "major mode for Rabbit" nil t)
 (add-to-list 'auto-mode-alist
              '("\\.\\(rbt\\|rab\\)$" . rabbit-mode)))
 
