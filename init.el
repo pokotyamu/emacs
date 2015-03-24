32fc18a97cd237659382ae6291d2ca0bcd5e1edf
pokotyamu <pokotyamu@gmail.com>
2015-03-24 16:07:58 +0900

    add markdown

--- init.el
+++ init.el
@@ -77,6 +77,7 @@
 ;;markdown記法の設定
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 
 ;;ruby & railsの基本的な設定
 (autoload 'ruby-mode "ruby-mode"
