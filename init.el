90b781778d467aa85db12df2fc00201e96e4a617
pokotyamu <pokotyamu@gmail.com>
2015-06-11 15:54:43 +0900

    Add coffee script mode

--- init.el
+++ init.el
@@ -209,3 +209,14 @@
 (add-to-list 'auto-mode-alist '("\\.jj\\'" . javacc-mode))
 (add-to-list 'auto-mode-alist '("\\.jjt\\'" . javacc-mode))
 
+
+;; Coffee script mode
+(require 'coffee-mode)
+(defun coffee-custom ()
+    "coffee-mode-hook"
+      (and (set (make-local-variable 'tab-width) 2)
+                  (set (make-local-variable 'coffee-tab-width) 2))
+        )
+
+(add-hook 'coffee-mode-hook
+            '(lambda() (coffee-custom)))
